42f8d89aa6a890d2935ab3a396d835d17cf10dcf
Princess <cessvalerie@gmail.com>
2023-05-07 00:25:10 +0600

    write test method for isNumericValue in test_project.py file

--- test_project.py
+++ test_project.py
@@ -3,11 +3,14 @@ import pandas as pd
 import pytest
 
 
-"""
-@pytest.mark.parametrize("dataframe, expected", [
-    ("",True),
-    ("02-02-2023 00:00:00",False)
-])"""
+
+@pytest.mark.parametrize("x, expected", [
+    ("70.2",True),
+    ("-120",False)
+])
+def test_isNumericValue(x, expected):
+    assert project.isNumericValue(x) == expected
+
 def test_identify_cols_types():
     data = {"calories": [420, 380, 390,500,100,200,300,150,450,1000,740,750,780,800],"duration": [40,78,99,15,20,50, 40, 45,1,2,30,100,4,50]}
 
